a04fd22342fc53669971c817bcff90df156b054d
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-09-13 16:03:34 +0100

    major homepage changes

--- src/components/VillajaHeader/VillajaHeader.jsx
+++ src/components/VillajaHeader/VillajaHeader.jsx
@@ -4,37 +4,52 @@ import searchIcon from '../../assets/search_icon.svg'
 import profileIcon from '../../assets/profile_icon.svg'
 import cartIcon from '../../assets/cart_icon.svg'
 import menuBtn from '../../assets/menu_btn.svg'
+import VillajaHeaderDropdown from './VillajaHeaderDropdown'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 
 const VillajaHeader = () => {
+
+    const [dropdownHoverState,setHoverState] = useState(0)
+
   return (
     <div className="vh-header-container">
         <div className="vh-item vh-logo">
-            <img src={villajaLogo} alt="" />
+           <Link to={'/'}>
+                <img src={villajaLogo} alt="" />
+            </Link>
         </div>
 
         <div className="vh-menu">
-            <div className="vh-item vh-menu-item">Phones</div>
-            <div className="vh-item vh-menu-item">Tablets</div>
-            <div className="vh-item vh-menu-item">Computers</div>
-            <div className="vh-item vh-menu-item">Accessories</div>
+            <div className="vh-item vh-menu-item" onMouseOver={() => setHoverState(1)} onMouseOut={() => setHoverState(0)}> <div>Phones</div> 
+            {(dropdownHoverState === 1) && <VillajaHeaderDropdown categoryNames={["Basic Phones","Smart Phones"]}/>}
+            </div>
+            <div className="vh-item vh-menu-item" onMouseOver={() => setHoverState(2)} onMouseOut={() => setHoverState(0)} >Tablets
+            {(dropdownHoverState === 2) && <VillajaHeaderDropdown categoryNames={["Professional Tablets","Educational Tablets"]}/>} </div>
+            <div className="vh-item vh-menu-item" onMouseOver={() => setHoverState(3)} onMouseOut={() => setHoverState(0)}>Computers
+            {(dropdownHoverState === 3) &&<VillajaHeaderDropdown categoryNames={["Laptops","Desktops"]}/>} </div>
+            <div className="vh-item vh-menu-item" onMouseOver={() => setHoverState(4)} onMouseOut={() => setHoverState(0)}>Accessories
+            {(dropdownHoverState === 4) && <VillajaHeaderDropdown categoryNames={["EarHeadphones","Smart Watches","Speakers","Micropphones","Chargers","Phone Cases","Storage Devices","Gaming Devices","Keyboards & Mice","Laptop Bags","Stands & Lights","Sytlus & Tablets"]}/> }</div>
             <div className="vh-item vh-menu-item">Support</div>
         </div>
 
         <div className="vh-aux-menu">
             <div className="vh-item vh-aux-item">
-                <img src={searchIcon} alt="search icon" />
+                <img src={searchIcon} alt="search icon" className='aux-img'/>
+            </div>
+            <div className="vh-item vh-aux-item" >
+                <img src={cartIcon} alt="cart icon" className='aux-img' />
             </div>
             <div className="vh-item vh-aux-item">
-                <img src={cartIcon} alt="cart icon" />
-            </div>
-            <div className="vh-item vh-aux-item">
-                <img src={profileIcon} alt="profile icon" />
+                <img src={profileIcon} alt="profile icon" className='aux-img' />
             </div>
         </div>
 
+        
+
         <div className="mobile-menu">
-            <img src={menuBtn} alt="menu button" />
+            <img src={menuBtn} alt="menu button" className='aux-img'/>
         </div>
     </div>
   )
