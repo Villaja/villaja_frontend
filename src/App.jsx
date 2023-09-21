1e2f1d5fc6aa407e9181d4a69380793a929def0c
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-09-21 20:16:59 +0100

    sorted daniel merge; customer section

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         <Routes>
           <Route exact path= "/" element={<CustomerHomePage/>}/>
           <Route exact path= "/login" element={<CustomerLogin/>}/>
+          <Route exact path= "/signup" element={<CustomerSignUp/>}/>
           <Route exact path= "/catalog/:id" element={<CatalogPage setSelectedCatalogCategory={setSelectedCatalogCategory}/>}/>
           <Route exact path= "/catalog/filter" element={<CategoryComponent category={selectedCatalogCategory} isMobile={'true'}/>}/>
           <Route  path= "/seller/*" element={<Seller setIsSeller={setIsSeller}/>}/>
