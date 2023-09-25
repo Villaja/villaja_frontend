7481b4815f2e33dca13f92c24a94d8c0e40ed302
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-09-25 12:11:24 +0100

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import CustomerSignUp from "../pages/Customer Signup Page/CustomerSignup";   
+import CustomerSignUp from "../pages/Customer SignUp Page/CustomerSignUp";   
 import CustomerLogin from "../pages/Customer Login Page/CustomerLogin";   
 import SellerSignUp from "../pages/Seller Signup Page/SellerSignUp";
 import SellerLogin from "../pages/Seller Login Page/SellerLogin";
