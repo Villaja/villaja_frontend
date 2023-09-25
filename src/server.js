a37256f3e6ebe4c0977cd9197aab257f2f2da8cf
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-09-25 11:52:37 +0100

    api link

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-// export const server = "https://extinct-blue-duckling.cyclic.app/api"
+export const server = "https://extinct-blue-duckling.cyclic.app/api"
 
-export const server = "http://localhost:8000/api"
+// export const server = "http://localhost:8000/api"
