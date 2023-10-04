7f342042409ec9cd0b23c137487629551101c61e
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-10-04 08:40:14 +0100

    Revert "Fixed dropdown isse VillajaHeader.jsx"
    This reverts commit 9c64c2e6c578a9dc368db538b1c3e0dc23b115ce.

--- src/components/VillajaHeader/VillajaHeader.jsx
+++ src/components/VillajaHeader/VillajaHeader.jsx
@@ -1,93 +1,51 @@
-import React, { useState } from 'react';
-import './villajaHeader.css';
-import villajaLogo from '../../assets/villaja_logo.svg';
-import searchIcon from '../../assets/search_icon.svg';
-import profileIcon from '../../assets/profile_icon.svg';
-import cartIcon from '../../assets/cart_icon.svg';
-import menuBtn from '../../assets/menu_btn.svg';
-import VillajaHeaderDropdown from './VillajaHeaderDropdown';
-import { Link, useLocation } from 'react-router-dom';
+import './villajaHeader.css'
+import villajaLogo from '../../assets/villaja_logo.svg'
+import searchIcon from '../../assets/search_icon.svg'
+import profileIcon from '../../assets/profile_icon.svg'
+import cartIcon from '../../assets/cart_icon.svg'
+import menuBtn from '../../assets/menu_btn.svg'
+import VillajaHeaderDropdown from './VillajaHeaderDropdown'
+import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
+
 
 const VillajaHeader = () => {
-  const [dropdownHoverState, setHoverState] = useState(0);
-  const location = useLocation();
 
-  const submitHandle = (e) => {
-    // Define the logic for handling menu item clicks here
-  };
+    const [dropdownHoverState,setHoverState] = useState(0)
+
+    const location = useLocation()
 
   return (
-    <div className="vh-header-wrapper" style={{ display: ['seller', 'login', 'signup'].includes(location.pathname.split('/')[1]) ? 'none' : 'block' }}>
-      <div className="vh-header-container">
+    <div className="vh-header-wrapper" style={location.pathname.split('/')[1]=='seller' || location.pathname.split('/')[1]=='login' || location.pathname.split('/')[1]=='signup' ?{display:"none"}:null}>
+
+    <div className="vh-header-container">
         <div className="vh-item vh-logo">
-          <Link to={'/'}>
-            <img src={villajaLogo} alt="" />
-          </Link>
+           <Link to={'/'}>
+                <img src={villajaLogo} alt="" />
+            </Link>
         </div>
 
-        <div className="vh-menu">
-          <div
-            className="vh-item vh-menu-item"
-            onMouseOver={() => setHoverState(1)}
-            onMouseOut={() => setHoverState(0)}
-            onClick={submitHandle}
-          >
-            Phones
-            {dropdownHoverState === 1 && <VillajaHeaderDropdown categoryNames={['Basic Phones', 'Smart Phones']} />}
-          </div>
-          <div
-            className="vh-item vh-menu-item"
-            onMouseOver={() => setHoverState(2)}
-            onMouseOut={() => setHoverState(0)}
-            onClick={submitHandle}
-          >
-            Tablets &nbsp;
-            {dropdownHoverState === 2 && <VillajaHeaderDropdown categoryNames={['Professional Tablets', 'Educational Tablets']} />}
-          </div>
-          <div
-            className="vh-item vh-menu-item"
-            onMouseOver={() => setHoverState(3)}
-            onMouseOut={() => setHoverState(0)}
-            onClick={submitHandle}
-          >
-            Computers &nbsp;
-            {dropdownHoverState === 3 && <VillajaHeaderDropdown categoryNames={['Laptops', 'Desktops']} />}
-          </div>
-          <div
-            className="vh-item vh-menu-item"
-            onMouseOver={() => setHoverState(4)}
-            onMouseOut={() => setHoverState(0)}
-            onClick={submitHandle}
-          >
-            Accessories &nbsp;
-            {dropdownHoverState === 4 && (
-              <VillajaHeaderDropdown
-                categoryNames={[
-                  'EarHeadphones',
-                  'Smart Watches',
-                  'Speakers',
-                  'Micropphones',
-                  'Chargers',
-                  'Phone Cases',
-                  'Storage Devices',
-                  'Gaming Devices',
-                  'Keyboards & Mice',
-                  'Laptop Bags',
-                  'Stands & Lights',
-                  'Sytlus & Tablets',
-                ]}
-              />
-            )}
-          </div>
-          <div className="vh-item vh-menu-item">Support</div>
+       <div className="vh-menu">
+            <div className="vh-item vh-menu-item" onMouseOver={() => setHoverState(1)} onMouseOut={() => setHoverState(0)} onClick={(e) => submitHandle(e)}> Phones
+            {(dropdownHoverState === 1) && <VillajaHeaderDropdown categoryNames={["Basic Phones","Smart Phones"]}/>}
+            </div>
+            <div className="vh-item vh-menu-item" onMouseOver={() => setHoverState(2)} onMouseOut={() => setHoverState(0)} onClick={(e) => submitHandle(e)}>Tablets &nbsp;
+            {(dropdownHoverState === 2) && <VillajaHeaderDropdown categoryNames={["Professional Tablets","Educational Tablets"]}/>} </div>
+            <div className="vh-item vh-menu-item" onMouseOver={() => setHoverState(3)} onMouseOut={() => setHoverState(0)} onClick={(e) => submitHandle(e)}>Computers  &nbsp;
+            {(dropdownHoverState === 3) &&<VillajaHeaderDropdown categoryNames={["Laptops","Desktops"]}/>} </div>
+            <div className="vh-item vh-menu-item" onMouseOver={() => setHoverState(4)} onMouseOut={() => setHoverState(0)} onClick={(e) => submitHandle(e)}>Accessories &nbsp;
+            {(dropdownHoverState === 4) && <VillajaHeaderDropdown categoryNames={["EarHeadphones","Smart Watches","Speakers","Micropphones","Chargers","Phone Cases","Storage Devices","Gaming Devices","Keyboards & Mice","Laptop Bags","Stands & Lights","Sytlus & Tablets"]}/> }</div>
+            <div className="vh-item vh-menu-item">Support</div>
         </div>
+        
 
         <div className="mobile-menu">
-          <img src={menuBtn} alt="menu button" className="aux-img" />
+            <img src={menuBtn} alt="menu button" className='aux-img'/>
         </div>
-      </div>
     </div>
-  );
-};
+    </div>
 
-export default VillajaHeader;
+  )
+}
+
+export default VillajaHeader
