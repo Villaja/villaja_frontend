d7eedb05646093ff20235c0db24fb76297a11c44
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-10-04 08:39:50 +0100

    Revert "Update VillajaHeaderDropdown.jsx"
    This reverts commit 64e2bec64bbb439f8b184f1cf41c17cbcdb67bf7.

--- src/components/VillajaHeader/VillajaHeaderDropdown.jsx
+++ src/components/VillajaHeader/VillajaHeaderDropdown.jsx
@@ -1,33 +1,20 @@
-import React, { useState } from 'react';
-import './villajaHeaderDropdown.css';
+import './villajaHeaderDropdown.css'
 
-const VillajaHeaderDropdown = ({ categoryNames }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsOpen(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsOpen(false);
-  };
-
+const VillajaHeaderDropdown = ({categoryNames}) => {
   return (
-    <div
-      className={`dropdown-container ${isOpen ? 'open' : ''}`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <div className="dc-header">Sub categories</div>
-      <div className="dc-main">
-        {categoryNames.map((name, id) => (
-          <div className="dc-main-cat" key={id}>
-            {name}
-          </div>
-        ))}
-      </div>
+    <div className="dropdown-container">
+        <div className="dc-header">Sub categories</div>
+        <div className="dc-main">
+          {
+            categoryNames.map((name,id) => {
+              return <div className="dc-main-cat" key={id}>
+                {name}
+              </div> 
+            })
+          }
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default VillajaHeaderDropdown;
+export default VillajaHeaderDropdown
