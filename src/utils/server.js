9683265f1076931e828d17f37e26f71cd6c2df61
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-10-12 13:01:24 +0100

    api update

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,3 +1,3 @@
-export const server = "https://tame-gray-bluefish-hat.cyclic.cloud/api"
+export const server = "https://fair-blue-turtle-tam.cyclic.app/api"
 
 // export const server = "http://localhost:8000/api"
