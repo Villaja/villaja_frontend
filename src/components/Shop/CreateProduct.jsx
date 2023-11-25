b72fae8f7903857b04883d1c80acaa54157f733d
Fasiku Daniel <131808326+daniel-fasiku@users.noreply.github.com>
2023-11-25 21:28:26 +0100

    comment changes

--- src/components/Shop/CreateProduct.jsx
+++ src/components/Shop/CreateProduct.jsx
@@ -269,7 +269,7 @@ const CreateProduct = () => {
           value={discountPrice}
           className="mt-2 appearance-none block w-full px-3 py-3 border border-gray-300 rounded-[6px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
           onChange={(e) => setDiscountPrice(e.target.value)}
-          placeholder="Enter Discount Price..."
+          placeholder="Enter Discount Price if any..."
           
         />
       </div>
