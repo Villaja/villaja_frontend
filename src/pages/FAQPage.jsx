c69426f7aaf576a9b262ef2b2fb84695735fb760
Fasiku Daniel <131808326+daniel-fasiku@users.noreply.github.com>
2023-12-24 15:51:45 +0100

    faq info changes

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -71,10 +71,10 @@ const Faq = () => {
           {activeTab === 2 && (
             <div className="mt-4">
               <p className="text-base text-gray-500">
-                If you're not satisfied with your purchase, we accept returns
-                within 30 days of delivery. To initiate a return, please email
-                us at support@myecommercestore.com with your order number and a
-                brief explanation of why you're returning the item.
+                If you are not satisfied with your purchase, we accept returns
+                within 24hrs of delivery. To initiate a return, please email
+                us at villajamarketplace@gmail.com with your order number and a
+                brief explanation of why you are returning the item with picture proof.
               </p>
             </div>
           )}
@@ -170,8 +170,8 @@ const Faq = () => {
           {activeTab === 4 && (
             <div className="mt-4">
               <p className="text-base text-gray-500">
-                You can contact our customer support team by emailing us at
-                support@myecommercestore.com, or by calling us at (555) 123-4567
+                You can contact our villaja support team by emailing us at
+                villajamarketplace@gmail.com, or text us on Instagram @villajatech
                 between the hours of 9am and 5pm EST, Monday through Friday.
               </p>
             </div>
@@ -221,7 +221,7 @@ const Faq = () => {
               <p className="text-base text-gray-500">
                 Unfortunately, once an order has been placed, we are not able to
                 make changes or cancellations. If you no longer want the items
-                you've ordered, you can return them for a refund within 30 days
+                you have ordered, you can return them for a refund within 30 days
                 of delivery.
               </p>
             </div>
@@ -269,7 +269,7 @@ const Faq = () => {
           {activeTab === 6 && (
             <div className="mt-4">
               <p className="text-base text-gray-500">
-                Currently, we only offer shipping within the United States.
+                Currently, we only offer shipping within Nigeria.
               </p>
             </div>
           )}
@@ -316,8 +316,8 @@ const Faq = () => {
           {activeTab === 7 && (
             <div className="mt-4">
               <p className="text-base text-gray-500">
-                We accept visa,mastercard,paypal payment method also we have
-                cash on delivery system.
+                We accept visa,mastercard,paypal payment method.Unfortunately we do not accept
+                cash on delivery payment at the moment.
               </p>
             </div>
           )}
