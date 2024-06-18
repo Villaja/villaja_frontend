5c8d64205f71bb9d2d889184ba166170ba743b08
Fasiku Daniel <daniel.fasiku@stu.cu.edu.ng>
2024-06-18 07:40:35 +0100

    server url update

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,5 +1,5 @@
 // export const server = "https://api-villaja.cyclic.app/api"
-export const server = "https://villaja-backend-servers.onrender.com/api"
+export const server = "https://villaja-server.onrender.com/api"
 
 // export const server = "http://localhost:8000/api"
 
