3b86f567178d1ff5d8a796602416b5c5a84f997a
Daniel Fasiku <adewolefas@gmail.com>
2024-11-01 00:01:11 +0100

    server changes

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 // export const server = "https://api-villaja.cyclic.app/api"
-export const server = "https://villaja-server.onrender.com/api"
+export const server = "https://villaja-backend-server.onrender.com/api"
 
 // export const server = "http://localhost:8000/api"
