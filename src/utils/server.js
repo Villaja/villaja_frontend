1f29507587e6ca440a1a89700cdc468225d4faf3
Daniel Fasiku <adewolefas@gmail.com>
2024-11-04 07:59:52 +0100

    switched server again to b4a

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,5 +1,5 @@
 // export const server = "https://api-villaja.cyclic.app/api"
-export const server = "https://villaja-backend-server.onrender.com/api"
+export const server = "https://villajabackendserver-48y0h87u.b4a.run/api"
 
 // export const server = "http://localhost:8000/api"
 
