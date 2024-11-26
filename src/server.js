9df50739a29083ecb26d4af0a4e9f4de342bc809
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2024-11-26 12:11:33 +0100

    update server url

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 // export const server = "https://api-villaja.cyclic.app/api"
-// export const server = "https://villajabackendserver-48y0h87u.b4a.run/api"
-// export const server = "https://3a3b-102-216-201-53.ngrok-free.app/api"
+export const server = "https://villajabackendserver-48y0h87u.b4a.run/api"
 
-export const server = "http://localhost:8000/api"
+// export const server = "http://localhost:8000/api"
